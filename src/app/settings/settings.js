76ad164317b0cceae46be5e524a97c292a913f39
Amrender9798 <amrender9798@gmail.com>
2024-03-31 08:24:25 +0530

    changed

--- src/app/settings/settings.js
+++ src/app/settings/settings.js
@@ -28,7 +28,7 @@ export default function Settings() {
   };
 
   return (
-    <div className="flex justify-center items-center h-screen">
+    <div className="flex justify-center items-center h-[90vh]">
       <div className="w-full max-w-lg p-8 bg-white rounded-lg shadow-md">
         <h1 className="text-2xl font-semibold mb-4">Settings</h1>
         <form onSubmit={handleSubmit}>
