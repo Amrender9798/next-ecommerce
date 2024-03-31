3fc16f86d77858d675e6c307729eb8ebb31f0263
Amrender9798 <amrender9798@gmail.com>
2024-03-31 07:21:20 +0530

    variable changed

--- src/app/catalog/page.js
+++ src/app/catalog/page.js
@@ -1,14 +1,19 @@
 import Catalog from "./catalog";
 
+const API_URL = process.env.NEXT_PUBLIC_BASE_API_URL;
 async function getProducts() {
-  const res = await fetch("http:localhost:3000/api/products?limit=10&offset=0");
-  const res2 = await fetch("http:localhost:3000/api/total-items");
+  console.log(API_URL); 
+  const res = await fetch( `${API_URL}/api/products/?limit=10&offset=0`);
+  const res2 = await fetch(`${API_URL}/api/total-items`);
   const { products } = await res.json();
   const { items } = await res2.json();
   return { products, items };
 }
 
 export default async function Products() {
+  if(!API_URL){
+    return null;
+  }
   // Fetch data directly in a Server Component
   const { products,items } = await getProducts();
   // Forward fetched data to your Client Component
