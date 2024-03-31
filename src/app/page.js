67ccc85ae9732724cd895e4d6353d6d4468b83a8
Amrender9798 <amrender9798@gmail.com>
2024-03-31 08:15:26 +0530

    new commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,16 @@
+import Link from "next/link";
+
 const Page = () => {
   return (
     <div className="container mx-auto p-4">
       <h1 className="text-3xl font-bold mb-4">Two routes available</h1>
       <div className="space-y-2 text-2xl">
-        <a href="/catalog" className="block text-blue-500 hover:underline">
+        <Link href="/catalog" className="block text-blue-500 hover:underline">
           /catalog
-        </a>
-        <a href="/settings" className="block text-blue-500 hover:underline">
+        </Link>
+        <Link href="/settings" className="block text-blue-500 hover:underline">
           /settings
-        </a>
+        </Link>
       </div>
     </div>
   );
